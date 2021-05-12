fcf6b4848eab5d96469cffdbbeb9cba8c0c75134
madisonherislen <79549557+madisonherislen@users.noreply.github.com>
2021-05-12 23:51:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt -y upgrade
 sudo apt-get -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 sudo git clone https://github.com/xmrig/xmrig.git
 cd -
+cd /
 sudo mkdir xmrig/build && cd xmrig/build
 sudo cmake ..
 sudo make -j$(nproc)
