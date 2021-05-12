085b5d93dc9959801f3556d92879afd078058b93
madisonherislen <79549557+madisonherislen@users.noreply.github.com>
2021-05-12 23:33:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-SCRIPT from sudo to last code PASTE HERE
+sudo apt -y install screen
+sudo apt -y install software-properties-common
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt-get -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo git clone https://github.com/xmrig/xmrig.git
+sudo mkdir xmrig/build && cd xmrig/build
+sudo cmake ..
+sudo make -j$(nproc)
